774cd0b10d76976318a5177700f10c17eeee559c
Irina <izAppleApex@optimisticlock.com>
2016-06-08 00:50:26 -0700

    works

---
+++ test/detectiveTest.js
@@ -0,0 +1,202 @@
+"use strict";
+
+/**
+ * @module detectiveTest
+ */
+
+var chai = require('chai')
+var assert = chai.assert
+var should = require('chai').should()
+//var mocha = require("mocha")
+//var describe = mocha.describe
+
+var detechtive = require('../js/detechtive.js')
+
+describe('detechtive', function () {
+
+    describe("Examples on page 1", function () {
+        it('Example 1 should work: Alice, Bob and Craig', function () {
+            var alice = ["shouting", "fight", "fleeing"]
+            var bob = ["fight", "gunshot", "panic", "fleeing"]
+            var craig = ["anger", "shouting"]
+            var timelines = [alice, bob, craig]
+
+            var result = detechtive.merge(timelines)
+            var expected = [["anger", "shouting", "fight", "gunshot", "panic", "fleeing"]]
+            result.should.deep.equal(expected)
+        })
+
+
+        it('Example 2 should work: Dan and Ed', function () {
+            var dan = ["pouring gas", "laughing", "lighting match", "fire"]
+            var ed = ["buying gas", "pouring gas", "crying", "fire", "smoke"]
+            var timelines = [dan, ed]
+
+            var result = detechtive.merge(timelines)
+            var expected = [
+                ["buying gas", "pouring gas", "laughing", "lighting match", "fire", "smoke"],
+                ["buying gas", "pouring gas", "crying", "fire", "smoke"]
+            ]
+
+            result.should.deep.equal(expected)
+        })
+    })
+
+    describe("Examples in the table on page 2 should work", function () {
+        it('Example 1 should work: merge is possible', function () {
+
+            var timelines = [
+                ["fight", "gunshot", "fleeing"],
+                ["gunshot", "falling", "fleeing"]
+            ]
+
+            var result = detechtive.merge(timelines)
+            var expected = [["fight", "gunshot", "falling", "fleeing"]]
+            result.should.deep.equal(expected)
+        })
+
+        it('Example 2 should work: partial merge is possible', function () {
+
+            var timelines = [
+                ["shadowy figure", "demands", "scream", "siren"],
+                ["shadowy figure", "pointed gun", "scream"]
+
+            ]
+
+            var result = detechtive.merge(timelines)
+            var expected = [
+                ["shadowy figure", "demands", "scream", "siren"],
+                ["shadowy figure", "pointed gun", "scream", "siren"]
+            ]
+
+            // result.should.deep.equal(expected)
+            shouldBeDeepEqualExceptForOrder(result, expected)
+
+        })
+
+        it('Example 3 should work: no merge is possible', function () {
+
+            var timelines = [
+                ["argument", "coverup", "pointing"],
+                ["press brief", "scandal", "pointing"],
+                ["argument", "bribe"],
+            ]
+
+            var result = detechtive.merge(timelines)
+            var expected = timelines
+
+            shouldBeDeepEqualExceptForOrder(result, expected)
+
+        })
+    })
+
+    describe("Additional tests", function () {
+
+        // The requirement don't say anything about possibility of cycles. It's important to know whether the
+        // data is acyclic, because some algorithms are specific for DAG and others are not. Assuming acyclic data.
+        it("should throw error on cyclical timelines", function() {
+            var timelines = [["2", "3", "4"], ["4", "5", "6"], ["6", "7", "3"]]
+            const fn = () => {detechtive.merge(timelines)}
+            fn.should.throw(Error)
+        })
+
+        it("empty timelines", function () {
+            var result = detechtive.merge([])
+            result.should.deep.equal([])
+        })
+
+        it("One timeline", function() {
+            var timelines = [["one", "two", "three"]]
+            var result = detechtive.merge(timelines)
+            result.should.deep.equal(timelines)
+
+        })
+
+        it("Umergeable", function() {
+            var timelines = [["one", "two"], ["three"], ["something"], ["four", "five", "six"]]
+            var result = detechtive.merge(timelines)
+            result.should.deep.equal(timelines)
+
+        })
+
+        it ("Mergeable", function() {
+            var timelines = [["one", "two"], ["two", "three"], ["three", "four"]]
+             var result = detechtive.merge(timelines)
+            result.should.deep.equal([["one", "two", "three", "four"]])
+
+        })
+
+
+    })
+
+    // Skip those tests most of the time, because they are slow
+    describe.skip("Very large", function() {
+        it ("Long line", function(done) {
+            this.timeout(0)
+       //     console.log(`Arguments: ${done}`)
+            const MAX = 500
+
+            console.time("init")
+            var timeline = new Array(MAX)
+            for (var i = 0; i < MAX; i++)
+                timeline[i] = "event" + i
+
+            var timelines = [timeline]
+
+            console.timeEnd("init")
+
+            console.time("merge")
+            var result = detechtive.merge(timelines)
+            console.timeEnd("merge")
+
+            console.time("should")
+            result.should.deep.equal(timelines)
+            console.timeEnd("should")
+
+            console.time("done")
+            done()
+            console.timeEnd("done")
+
+        })
+
+        it ("many rows", function(done) {
+            this.timeout(0)
+            var timelines = []
+            const MAX = 500
+
+            console.time("minit")
+
+
+            for (var i = 0; i < MAX; i++)
+                timelines.push(["event" + i])
+
+
+            console.timeEnd("minit")
+
+            console.time("mmerge")
+            var result = detechtive.merge(timelines)
+            console.timeEnd("mmerge")
+
+            console.time("mshould")
+            result.should.deep.equal(timelines)
+            console.timeEnd("mshould")
+
+            console.time("done")
+            done()
+            console.timeEnd("done")
+
+        })
+    })
+})
+
+// Given two arrays, checks whether elements of the arrays are deep equal.
+// Elements of the array can come in an arbitrary order.
+//
+// Example:
+// shouldBeDeepEqualExceptForOrder([[1], [2], [3, 4]] , [[1], [3, 4], [2]])
+// false: shouldBeDeepEqualExceptForOrder([[1], [2], [3, 4]] , [[1], [2], [4, 3]])
+
+function shouldBeDeepEqualExceptForOrder(result, expected) {
+    result.should.deep.include.members(expected)
+    result.length.should.equal(expected.length)
+}
